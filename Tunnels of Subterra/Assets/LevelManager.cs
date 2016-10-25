de6eabe880ec43f7c0e4042ea23e647209e8da13
csmacdonald95 <csmacdonald95@gmail.com>
2016-10-25 16:36:19 -0300

    Start level manager and obstacle manager
    - Script to decide when obstacles and enemies are generated
    - Script to instantiate obstacles when they are called
    - Script to move obstacles and delete them when they have travelled
    fully

---
+++ Tunnels of Subterra/Assets/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public float levelSpeed = 1.5f;
+    public float obstacleProbability = 0.05f;
+    public int maxObstacles = 3;
+    public float enemyProbability = 0.05f;
+    public int maxEnemies = 2;
+
+	void Start () {
+        InvokeRepeating("chooseElements", 2.0f, 0.1f);
+	}
+
+    float getRand() {
+        return Random.Range(0.0f, 1.0f);
+    }
+
+    void chooseElements() {
+        while(true) {
+            if (getRand() <= obstacleProbability) {
+                //generateObstacle();
+            }
+            if (getRand() <= enemyProbability) {
+                //generateEnemy();
+            }
+        }
+    }
+}
+
--- Tunnels of Subterra/Assets/OffsetMaterial.cs
+++ Tunnels of Subterra/Assets/OffsetMaterial.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class OffsetMaterial : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public float scrollSpeed = 0.5F;
+    public Renderer rend;
+    void Start() {
+        rend = GetComponent<Renderer>();
+    }
+    void Update() {
+        float offset = Time.time * scrollSpeed;
+        rend.material.mainTextureOffset = new Vector3(0, offset, 0);
+    }
 }
