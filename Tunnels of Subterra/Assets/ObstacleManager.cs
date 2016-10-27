1eb6e3d403ca5ea57a24bef9dfd231ae2c7df910
csmacdonald95 <csmacdonald95@gmail.com>
2016-10-27 13:21:24 -0300

    Add improved obstacle generation
    Obstacles will now appear both of the floor and ceiling and will be
    varied along the X-axis (side to side). Moreover, the an obstacle
    manager has been added which can allow new obstacles to be easily
    implemented.

---
+++ Tunnels of Subterra/Assets/ObstacleManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObstacleManager : MonoBehaviour {
+
+	[System.Serializable]
+	public class Obstacle {
+
+		public GameObject obstacleObject;
+		public Vector3 roughPosition;
+		public Vector3 orientation;
+		public float xVariance;
+
+		public Obstacle(GameObject obstacleObject, Vector3 roughPosition, 
+						float xVariance, Vector3 orientation) {
+			this.obstacleObject = obstacleObject;
+			this.roughPosition = roughPosition;
+			this.xVariance = xVariance;
+			this.orientation = orientation;
+		}
+
+		public void placeObstacle() {
+			Vector3 pos = roughPosition + new Vector3(Random.Range(-xVariance, xVariance), 0, 0);
+			Instantiate(obstacleObject, pos, Quaternion.Euler(orientation));
+		}
+	}
+
+	public List<Obstacle> obstacles;
+
+	public void generateObstacle() {
+		Obstacle currentObstacle = obstacles[Random.Range(0, obstacles.Count)];
+		if (currentObstacle.obstacleObject != null)
+			currentObstacle.placeObstacle();
+	}
+}
