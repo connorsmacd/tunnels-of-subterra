3e7e9bd3c538d933b93de943379e1c8b63961295
csmacdonald95 <csmacdonald95@gmail.com>
2016-10-31 22:00:21 -0300

    Add tunnel manager
    tunnel is now made up of repeating tunnel segments that are generated as
    the player moves

---
+++ Tunnels of Subterra/Assets/MovePlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour {
+
+    private float playerSpeed;
+
+    void Start() {
+        playerSpeed = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<LevelManager>().levelSpeed;
+    }
+
+    void Update() {
+        transform.Translate(0, 0, playerSpeed * Time.deltaTime);
+    }
+}
--- Tunnels of Subterra/Assets/TunnelManager.cs
+++ Tunnels of Subterra/Assets/TunnelManager.cs
@@ -1,21 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class TunnelManager : MonoBehaviour { 
+public class TunnelManager : MonoBehaviour {
 
-	public GameObject 
-	private Vector3 cameraPosition;
-	private	Renderer tunnelRenderer;
+    public GameObject tunnelObjectNormal;
+    public GameObject tunnelObjectReflected;
+    private Queue<GameObject> segmentQueue = new Queue<GameObject>();
+    private bool reflect;
+	private float frontFogZ;
+    private float rearFogZ;
+    private float currentSegmentZ;
+    private float segmentLength;
+
+    void addNewSegment() {
+        currentSegmentZ += segmentLength;
+        Vector3 position = new Vector3(0, 0, currentSegmentZ);
+        GameObject newSegment;
+        if (reflect) {
+            newSegment = tunnelObjectNormal;
+            reflect = false;
+        } else {
+            newSegment = tunnelObjectReflected;
+            reflect = true;
+        }
+        Instantiate(newSegment, position, new Quaternion());
+    }
 
 	void Start () {
-		cameraPosition = GameObject.FindGameObjectWithTag("Fog")
-											.transform.position;
-		tunnelRenderer = GameObject.FindGameObjectWithTag("Tunnel")
-											   .transform.position;
-	}
+        Instantiate(tunnelObjectNormal, new Vector3(0, 0, 0), new Quaternion());
+        segmentQueue.Enqueue(tunnelObjectNormal);
+        frontFogZ = GameObject.FindGameObjectWithTag("Player").transform.FindChild("FrontFog").transform.position.z;
+        rearFogZ = GameObject.FindGameObjectWithTag("Player").transform.FindChild("RearFog").transform.position.z;
+        reflect = false;
+        currentSegmentZ = 0.0f;
+        segmentLength = segmentQueue.Peek().transform.GetChild(2).GetComponent<MeshRenderer>().bounds.size.y;
+    }
 
 	void Update () {
-		if (
+        frontFogZ = GameObject.FindGameObjectWithTag("Player").transform.FindChild("FrontFog").transform.position.z;
+        if ((frontFogZ - currentSegmentZ) >= (segmentLength / 2)) {
+            addNewSegment();
+        }
 	}
 }
 	
