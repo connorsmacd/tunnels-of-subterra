8708d681e80adc4f92f1d365d5fb54c8329bcf5b
csmacdonald95 <csmacdonald95@gmail.com>
2016-11-01 22:27:28 -0300

    Add new obstacle manager and obstacles
    Obstacle manager will now work with the new tunnel system. Obstacles are
    actual stalactites and stalagmites now.

--- Tunnels of Subterra/Assets/ObstacleManager.cs
+++ Tunnels of Subterra/Assets/ObstacleManager.cs
@@ -5,32 +5,38 @@ using System.Collections.Generic;
 public class ObstacleManager : MonoBehaviour {
 
 	[System.Serializable]
-	public class Obstacle {
-
+	public struct Obstacle {
 		public GameObject obstacleObject;
-		public Vector3 roughPosition;
+		public Vector2 roughPosition;
 		public Vector3 orientation;
 		public float xVariance;
-
-		public Obstacle(GameObject obstacleObject, Vector3 roughPosition, 
-						float xVariance, Vector3 orientation) {
-			this.obstacleObject = obstacleObject;
-			this.roughPosition = roughPosition;
-			this.xVariance = xVariance;
-			this.orientation = orientation;
-		}
-
-		public void placeObstacle() {
-			Vector3 pos = roughPosition + new Vector3(Random.Range(-xVariance, xVariance), 0, 0);
-			Instantiate(obstacleObject, pos, Quaternion.Euler(orientation));
-		}
 	}
 
-	public List<Obstacle> obstacles;
+	public List<Obstacle> obstacles; 
+    public float distanceFromFog = 5.0f;
+    public GameObject parent;
 
-	public void generateObstacle() {
+    private Queue<GameObject> obstacleObjectQueue = new Queue<GameObject>();
+
+    public void generateObstacle() {
 		Obstacle currentObstacle = obstacles[Random.Range(0, obstacles.Count)];
-		if (currentObstacle.obstacleObject != null)
-			currentObstacle.placeObstacle();
+		if (currentObstacle.obstacleObject != null) {
+            float frontFogZ = GameObject.FindGameObjectWithTag("Player").transform.FindChild("FrontFog").transform.position.z;
+            Vector3 position = (Vector3) currentObstacle.roughPosition 
+                               + new Vector3(Random.Range(-currentObstacle.xVariance, currentObstacle.xVariance), 0, frontFogZ + distanceFromFog);
+            GameObject currentObstacleObject = (GameObject) Instantiate(currentObstacle.obstacleObject, 
+                                                                        position, Quaternion.Euler(currentObstacle.orientation));
+            currentObstacleObject.transform.parent = parent.transform;
+            obstacleObjectQueue.Enqueue(currentObstacleObject);
+        }
 	}
+
+    void Update () {
+        float rearFogZ = GameObject.FindGameObjectWithTag("Player").transform.FindChild("RearFog").transform.position.z;
+        if (obstacleObjectQueue.Count > 0) {
+            if ((rearFogZ - obstacleObjectQueue.Peek().transform.position.z) >= 0) {
+                Destroy(obstacleObjectQueue.Dequeue());
+            }
+        }
+    }
 }
