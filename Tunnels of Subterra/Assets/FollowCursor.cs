dbc1b75ca98feb04da3fc2869983d783eab370d6
csmacdonald95 <csmacdonald95@gmail.com>
2016-11-06 15:52:20 -0400

    Add temporary crosshair

---
+++ Tunnels of Subterra/Assets/FollowCursor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCursor : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray aimRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 point = aimRay.GetPoint(Vector3.Distance(gameObject.transform.position, aimRay.origin));
+        point.z = gameObject.transform.parent.transform.position.z;
+        gameObject.transform.position = point;
+    }
+}
